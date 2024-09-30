c22b00adafea20e961b5ef3fde6839d02b7a7164
oliviaworks <oliviagithub9@gmail.com>
2024-09-30 09:12:44 -0300

    Alterações nas procedures e no banco

--- 2_ano/postgresql/script_bd_relacional.sql
+++ 2_ano/postgresql/script_bd_relacional.sql
@@ -1,35 +1,32 @@
--- Criação do banco de dados e conexão
---CREATE DATABASE diversis;
--- Esta linha é usada apenas em psql e deve ser omitida se não estiver usando o cliente psql diretamente
---\c diversis
--- Criação das tabelas
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE tipo_perfil (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE situacao_trabalhista (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE setor (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE tipo_arquivo (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE tipo_vaga (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE endereco (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     rua VARCHAR(200) NOT NULL,
     estado VARCHAR(100) NOT NULL,
     cidade VARCHAR(100) NOT NULL,
@@ -38,113 +35,125 @@ CREATE TABLE endereco (
 );
 
 CREATE TABLE arquivo (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(150) NOT NULL,
     s3_url VARCHAR(300) NOT NULL,
     s3_key VARCHAR(50) NOT NULL,
     tamanho VARCHAR(50) NOT NULL,
-    fk_tipo_arquivo_id INT NOT NULL,
+    fk_tipo_arquivo_id UUID NOT NULL,
     FOREIGN KEY (fk_tipo_arquivo_id) REFERENCES tipo_arquivo (id)
 );
 
 CREATE TABLE perfil (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(100) NOT NULL,
-    senha VARCHAR(50) NOT NULL,
+    senha VARCHAR(8) NOT NULL, 
     email VARCHAR(100) UNIQUE NOT NULL,
     biografia VARCHAR(300),
-    fk_tipo_perfil_id INT NOT NULL,
-    fk_ft_perfil_id INT,
-    Foreign Key (fk_ft_perfil_id) REFERENCES arquivo (id),
+    fk_tipo_perfil_id UUID NOT NULL,
+    fk_ft_perfil_id UUID,
+    FOREIGN KEY (fk_ft_perfil_id) REFERENCES arquivo (id),
     FOREIGN KEY (fk_tipo_perfil_id) REFERENCES tipo_perfil (id)
 );
 
 CREATE TABLE telefone (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     telefone VARCHAR(11) NOT NULL,
-    fk_perfil_id INT NOT NULL,
+    fk_perfil_id UUID NOT NULL,
     FOREIGN KEY (fk_perfil_id) REFERENCES perfil (id)
 );
 
 CREATE TABLE usuario (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     cpf VARCHAR(11) UNIQUE NOT NULL,
-    fk_perfil_id INT NOT NULL,
-    fk_curriculo_id INT,
+    fk_perfil_id UUID NOT NULL,
+    fk_curriculo_id UUID,
     dt_nascimento DATE NOT NULL,
     pronomes VARCHAR(50) NOT NULL,
     nome_social VARCHAR(100),
     FOREIGN KEY (fk_perfil_id) REFERENCES perfil (id),
-    Foreign Key (fk_curriculo_id) REFERENCES arquivo (id),
+    FOREIGN KEY (fk_curriculo_id) REFERENCES arquivo (id)
 );
 
 CREATE TABLE empresa (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     cnpj VARCHAR(14) UNIQUE NOT NULL,
     razao_social VARCHAR(100) NOT NULL,
     website VARCHAR(220),
-    fk_perfil_id INT NOT NULL,
-    fk_endereco_id INT NOT NULL,
-    fk_setor_id INT NOT NULL,
+    fk_perfil_id UUID NOT NULL,
+    fk_endereco_id UUID NOT NULL,
+    fk_setor_id UUID NOT NULL,
     FOREIGN KEY (fk_setor_id) REFERENCES setor (id),
     FOREIGN KEY (fk_endereco_id) REFERENCES endereco (id),
     FOREIGN KEY (fk_perfil_id) REFERENCES perfil (id)
 );
 
 CREATE TABLE configuracao (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     notificacao BOOLEAN,
-    fk_perfil_id INT NOT NULL,
+    fk_perfil_id UUID NOT NULL,
     FOREIGN KEY (fk_perfil_id) REFERENCES perfil (id)
 );
 
 CREATE TABLE curso (
-    id SERIAL PRIMARY KEY,
-    descricao VARCHAR(300) NOT NULL,
-    fk_perfil_id INT NOT NULL,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    descricao VARCHAR(300),
+    fk_perfil_id UUID NOT NULL,
     nome VARCHAR(100) NOT NULL,
-    FOREIGN KEY (fk_perfil_id) REFERENCES perfil (id),
+    FOREIGN KEY (fk_perfil_id) REFERENCES perfil (id)
 );
 
 CREATE TABLE material_curso (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome VARCHAR(255) NOT NULL,
-    fk_curso_id INT NOT NULL,
-    fk_arquivo_id INT NOT NULL,
+    fk_curso_id UUID NOT NULL,
+    fk_arquivo_id UUID NOT NULL,
     descricao VARCHAR(500),
     FOREIGN KEY (fk_curso_id) REFERENCES curso (id),
     FOREIGN KEY (fk_arquivo_id) REFERENCES arquivo (id)
 );
 
 CREATE TABLE inscricao_curso (
-    id SERIAL PRIMARY KEY,
-    fk_curso_id INT NOT NULL,
-    fk_usuario_id INT NOT NULL,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    fk_curso_id UUID NOT NULL,
+    fk_usuario_id UUID NOT NULL,
     FOREIGN KEY (fk_curso_id) REFERENCES curso (id),
     FOREIGN KEY (fk_usuario_id) REFERENCES usuario (id)
 );
 
 CREATE TABLE vaga (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     descricao VARCHAR(300) NOT NULL,
     nome VARCHAR(100) NOT NULL,
-    fk_empresa_id INT NOT NULL,
-    fk_tipo_vaga_id INT NOT NULL,
+    fk_empresa_id UUID NOT NULL,
+    fk_tipo_vaga_id UUID NOT NULL,
     FOREIGN KEY (fk_empresa_id) REFERENCES empresa (id),
     FOREIGN KEY (fk_tipo_vaga_id) REFERENCES tipo_vaga (id)
 );
 
 CREATE TABLE inscricao_vaga (
-    id SERIAL PRIMARY KEY NOT NULL,
-    fk_usuario_id INT NOT NULL,
-    fk_vaga_id INT NOT NULL,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    fk_usuario_id UUID NOT NULL,
+    fk_vaga_id UUID NOT NULL,
     FOREIGN KEY (fk_vaga_id) REFERENCES vaga (id),
     FOREIGN KEY (fk_usuario_id) REFERENCES usuario (id)
 );
 
 CREATE TABLE avaliacao_curso (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nota NUMERIC NOT NULL,
-    fk_curso_id INT REFERENCES curso(id),
-    fk_usuario_id INT REFERENCES usuario(id)
+    fk_curso_id UUID REFERENCES curso (id),
+    fk_usuario_id UUID REFERENCES usuario (id)
+);
+
+CREATE TABLE permissao_vaga (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    permissao BOOLEAN DEFAULT false,
+    id_vaga UUID REFERENCES vaga (id)
+);
+
+CREATE TABLE permissao_curso (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    permissao BOOLEAN DEFAULT false,
+    fk_curso_id UUID REFERENCES curso (id)
 );
--- 2_ano/postgresql/script_db_relacional_mock.sql
+++ 2_ano/postgresql/script_db_relacional_mock.sql
@@ -1,58 +1,94 @@
---tipo_perfil
-INSERT INTO tipo_perfil (nome) VALUES ('USUARIO'), ('EMPRESA');
-
---situacao_trabalhista
+-- Tabela tipo_perfil
 INSERT INTO
-    situacao_trabalhista (nome)
-VALUES ('DESEMPREGADO'),
-    ('EMPREGADO'),
-    ('AUTONOMO');
-
--- setor
-INSERT INTO
-    setor (nome)
-VALUES ('TECNOLOGIA DA INFORMAÇÃO'),
-    ('RECURSOS HUMANOS'),
-    ('FINANCEIRO');
-
---status_curso
-INSERT INTO
-    status_curso (nome)
-VALUES ('CONCLUIDO'),
-    ('EM ANDAMENTO'),
-    ('PENDENTE');
-
---tipo_arquivo
-INSERT INTO
-    tipo_arquivo (nome)
-VALUES ('CURRICULO'),
-    ('FOTO DE PERFIL'),
-    ('MATERIAL CURSO');
-
---turno
-INSERT INTO
-    turno (nome)
-VALUES ('MATUTINO'),
-    ('VESPERTINO'),
-    ('NOTURNO');
-
---tipo_vaga
-INSERT INTO tipo_vaga (nome) VALUES ('ABERTA'), ('FECHADA');
-
---endereco
-INSERT INTO
-    endereco (rua, estado, numero)
-VALUES ('Rua A', 'São Paulo', 123),
-    (
-        'Rua B',
-        'Rio de Janeiro',
-        456
+    tipo_perfil (id, nome)
+VALUES (
+        '2e1249e8-5731-4f7f-812e-83f18f25a79e',
+        'USUARIO'
     ),
-    ('Rua C', 'Minas Gerais', 789);
+    (
+        '9d8692b6-7092-4e3c-8610-9b5f591a0410',
+        'EMPRESA'
+    );
 
---arquivo
+-- Tabela situacao_trabalhista
+INSERT INTO
+    situacao_trabalhista (id, nome)
+VALUES (
+        '1f65bff6-7d4c-43e8-a493-9e8c4d4f6d7d',
+        'Empregado'
+    ),
+    (
+        '34f67c88-86c3-4ac9-b472-9df540ed963e',
+        'Desempregado'
+    );
+
+-- Tabela setor
+INSERT INTO
+    setor (id, nome)
+VALUES (
+        '53e4c6fd-23cf-487b-bd63-144db86b78d9',
+        'Tecnologia'
+    ),
+    (
+        '4b22b60b-2ef0-45cd-9444-3adcb9145a10',
+        'Saúde'
+    );
+
+-- Tabela tipo_arquivo
+INSERT INTO
+    tipo_arquivo (id, nome)
+VALUES (
+        'ff7b274b-cb4f-45b4-871d-7b51a38b1045',
+        'Imagem'
+    ),
+    (
+        'eaa1c85b-d31b-41b3-8048-47d89b33e35b',
+        'Documento'
+    );
+
+-- Tabela tipo_vaga
+INSERT INTO
+    tipo_vaga (id, nome)
+VALUES (
+        '11e72bfa-09c8-47f5-9f90-7bdf70a0e9c3',
+        'Home Office'
+    ),
+    (
+        'f908c11e-67cb-44b4-9436-0e7f30e11223',
+        'Presencial'
+    );
+
+-- Tabela endereco
+INSERT INTO
+    endereco (
+        id,
+        rua,
+        estado,
+        cidade,
+        cep,
+        numero
+    )
+VALUES (
+        'dde40cf8-7ed5-4b85-a3b5-38fe726d9096',
+        'Rua 1',
+        'SP',
+        'São Paulo',
+        '12345678',
+        100
+    ),
+    (
+        '9cbdfed1-4cc7-4f24-bf84-31f5784e1052',
+        'Rua 2',
+        'RJ',
+        'Rio de Janeiro',
+        '87654321',
+        200
+    );
+
+-- Tabela arquivo
 INSERT INTO
     arquivo (
+        id,
         nome,
         s3_url,
         s3_key,
@@ -60,30 +96,26 @@ INSERT INTO
         fk_tipo_arquivo_id
     )
 VALUES (
-        'Curriculo_Joao.pdf',
-        'https://s3.amazonaws.com/curriculos/Curriculo_Joao.pdf',
-        'Curriculo_Joao',
-        '200KB',
-        1
-    ),
-    (
-        'Foto_Maria.jpg',
-        'https://s3.amazonaws.com/fotos/Foto_Maria.jpg',
-        'Foto_Maria',
+        'ae86b38f-8995-4a85-8de9-c01a94baf1c3',
+        'foto_perfil1.jpg',
+        'https://s3.aws.com/foto1.jpg',
+        'key1',
         '500KB',
-        2
+        'ff7b274b-cb4f-45b4-871d-7b51a38b1045'
     ),
     (
-        'Material_Curso_01.pdf',
-        'https://s3.amazonaws.com/materiais/Material_Curso_01.pdf',
-        'Material_Curso_01',
-        '2MB',
-        3
+        'bc3db85c-17d1-4206-aec5-7c874957b29a',
+        'curriculo1.pdf',
+        'https://s3.aws.com/curriculo1.pdf',
+        'key2',
+        '300KB',
+        'eaa1c85b-d31b-41b3-8048-47d89b33e35b'
     );
 
---perfil
+-- Tabela perfil
 INSERT INTO
     perfil (
+        id,
         nome,
         senha,
         email,
@@ -92,55 +124,63 @@ INSERT INTO
         fk_ft_perfil_id
     )
 VALUES (
-        'João Silva',
+        'd73b4235-0b82-49f6-8361-94341b423d35',
+        'Pedro',
         'senha123',
-        'joao@email.com',
-        'Desenvolvedor de software com 5 anos de experiência',
-        2,
-        1
+        'pedro@email.com',
+        'Engenheiro de Software',
+        '2e1249e8-5731-4f7f-812e-83f18f25a79e',
+        'ae86b38f-8995-4a85-8de9-c01a94baf1c3'
     ),
     (
-        'Maria Oliveira',
-        'senha456',
-        'maria@email.com',
-        'Gerente de projetos apaixonada por inovação',
-        2,
-        2
+        '27b60de4-8b7c-4685-8bb7-08ad091b3b2f',
+        'TechCorp',
+        'senhaemp',
+        'contato@techcorp.com',
+        'Empresa de tecnologia',
+        '9d8692b6-7092-4e3c-8610-9b5f591a0410',
+        NULL
     );
 
---telefone
+-- Tabela telefone
 INSERT INTO
-    telefone (telefone, fk_perfil_id)
-VALUES ('11999999999', 1),
-    ('21888888888', 2);
+    telefone (id, telefone, fk_perfil_id)
+VALUES (
+        'f91d24d4-f1b9-4340-b154-69ec9b18ed85',
+        '11987654321',
+        'd73b4235-0b82-49f6-8361-94341b423d35'
+    ),
+    (
+        '2c5f1915-b195-42ad-8459-6b6f47a17cd0',
+        '21998765432',
+        '27b60de4-8b7c-4685-8bb7-08ad091b3b2f'
+    );
 
---usuario
+-- Tabela usuario
 INSERT INTO
     usuario (
+        id,
         cpf,
         fk_perfil_id,
-        fk_situacao_trabalhista_id,
         fk_curriculo_id,
-        dt_nascimento
+        dt_nascimento,
+        pronomes,
+        nome_social
     )
 VALUES (
+        'cb612ee4-0a25-4e98-a9e6-04a11c636ea5',
         '12345678901',
-        1,
-        2,
-        1,
-        '1985-01-15'
-    ),
-    (
-        '98765432100',
-        2,
-        1,
-        2,
-        '1990-06-25'
+        'd73b4235-0b82-49f6-8361-94341b423d35',
+        'bc3db85c-17d1-4206-aec5-7c874957b29a',
+        '1990-05-15',
+        'ele/dele',
+        'Pedro S.'
     );
 
---empresa
+-- Tabela empresa
 INSERT INTO
     empresa (
+        id,
         cnpj,
         razao_social,
         website,
@@ -149,108 +189,87 @@ INSERT INTO
         fk_setor_id
     )
 VALUES (
-        '12345678000100',
-        'Tech Solutions Ltda',
-        'https://techsolutions.com',
-        1,
-        1,
-        1
-    ),
-    (
-        '98765432000100',
-        'Financorp SA',
-        'https://financorp.com',
-        2,
-        2,
-        2
+        'b537af34-35be-470d-8222-7558f3e2f8c9',
+        '12345678000195',
+        'TechCorp',
+        'https://techcorp.com',
+        '27b60de4-8b7c-4685-8bb7-08ad091b3b2f',
+        'dde40cf8-7ed5-4b85-a3b5-38fe726d9096',
+        '53e4c6fd-23cf-487b-bd63-144db86b78d9'
     );
 
---configuracao
+-- Tabela configuracao
 INSERT INTO
-    configuracao (notificacao, fk_perfil_id)
-VALUES (TRUE, 1),
-    (FALSE, 2);
+    configuracao (id, notificacao, fk_perfil_id)
+VALUES (
+        'b7527d52-1109-4c32-93b7-52e7b5075195',
+        TRUE,
+        'd73b4235-0b82-49f6-8361-94341b423d35'
+    );
 
---curso
+-- Tabela curso
 INSERT INTO
     curso (
+        id,
         descricao,
         fk_perfil_id,
-        carga_horaria,
-        nome,
-        fk_status_curso_id
+        nome
     )
 VALUES (
-        'Curso de Programação em Python',
-        1,
-        40,
-        'Python para Iniciantes',
-        2
-    ),
-    (
-        'Curso de Gerenciamento de Projetos',
-        2,
-        60,
-        'PMP Essentials',
-        1
+        '7414f1b9-7d96-4f64-87d9-f6c610b857c9',
+        'Curso de Java',
+        'd73b4235-0b82-49f6-8361-94341b423d35',
+        'Java para Iniciantes'
     );
 
---material_curso
+-- Tabela material_curso
 INSERT INTO
     material_curso (
+        id,
         nome,
         fk_curso_id,
         fk_arquivo_id,
         descricao
     )
 VALUES (
-        'Apostila de Python',
-        1,
-        3,
-        'Material complementar para o curso de Python'
-    ),
-    (
-        'Slides do Curso PMP',
-        2,
-        3,
-        'Slides utilizados no curso de Gerenciamento de Projetos'
+        'd04c5f9d-65b4-4baf-bb67-bbf2125b1c38',
+        'Apostila de Java',
+        '7414f1b9-7d96-4f64-87d9-f6c610b857c9',
+        'bc3db85c-17d1-4206-aec5-7c874957b29a',
+        'Apostila completa do curso'
     );
 
---inscricao_curso
+-- Tabela inscricao_curso
 INSERT INTO
-    inscricao_curso (fk_curso_id, fk_usuario_id)
-VALUES (1, 1),
-    (2, 2);
-
---vaga
-INSERT INTO
-    vaga (
-        descricao,
-        emprego,
-        jornada,
-        salario,
-        fk_turno_id,
-        qnt_vagas,
-        fk_empresa_id,
-        fk_tipo_vaga
+    inscricao_curso (
+        id,
+        fk_curso_id,
+        fk_usuario_id
     )
 VALUES (
-        'Desenvolvedor Backend',
-        'Programador',
-        40,
-        6000,
-        1,
-        3,
-        1,
-        2
-    ),
-    (
-        'Analista Financeiro',
-        'Financeiro',
-        40,
-        5000,
-        2,
-        2,
-        2,
-        2
+        '9873a36d-2b8f-4a75-84b5-e36a9e9d3743',
+        '7414f1b9-7d96-4f64-87d9-f6c610b857c9',
+        'cb612ee4-0a25-4e98-a9e6-04a11c636ea5'
     );
+
+-- Tabela vaga
+INSERT INTO
+    vaga (
+        id,
+        descricao,
+        nome,
+        fk_empresa_id,
+        fk_tipo_vaga_id
+    )
+VALUES (
+        '39f72c56-73f3-4d7b-8c1d-bfd8f523b021',
+        'Desenvolvimento Java',
+        'Desenvolvedor Java Junior',
+        'b537af34-35be-470d-8222-7558f3e2f8c9',
+        '11e72bfa-09c8-47f5-9f90-7bdf70a0e9c3'
+    );
+
+-- Tabela inscricao_vaga
+INSERT INTO inscricao_vaga (id, fk_usuario_id, fk_vaga_id) VALUES
+('f87248b8-e72e-4725-b530-c7fc3f3fa32f', 'cb612ee4-0a25-4e98-a9e6-04a11c636ea5', '39f72c56-73f3-4d7b-8c1d-bfd8f523b021');
+
